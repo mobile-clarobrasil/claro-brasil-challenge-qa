eb5094d8205891f3a926e81278e431dbd088f2d5
Rodrigo Rosa <rodrigo.rosa@buson.com.br>
2022-03-21 18:06:31 -0300

    Implementing IOS_appium tests

--- APPIUM_PROJECT/AppiumChallenge/src/test/java/challenge/Test/ChallengeAppiumTests.java
+++ APPIUM_PROJECT/AppiumChallenge/src/test/java/challenge/Test/ChallengeAppiumTests.java
@@ -16,34 +16,50 @@ import org.openqa.selenium.remote.DesiredCapabilities;
 
 import io.appium.java_client.MobileElement;
 import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.ios.IOSDriver;
 
 public class ChallengeAppiumTests {
 
 	@SuppressWarnings("rawtypes")
-	private AndroidDriver driver;
+	//private AndroidDriver driver;
+	private IOSDriver driver;
 
 	@SuppressWarnings("rawtypes")
 	@BeforeEach
 	public void setUp() throws MalformedURLException {
 		try {
-			DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
+			/*DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
 			desiredCapabilities.setCapability("platformName", "Android");
 			desiredCapabilities.setCapability("deviceName", "emulator-5554");
 			desiredCapabilities.setCapability("automationName", "uiautomator2");
 			desiredCapabilities.setCapability("appPackage", "com.android.calculator2");
-			desiredCapabilities.setCapability("appActivity", "com.android.calculator2.Calculator");
+			desiredCapabilities.setCapability("appActivity", "com.android.calculator2.Calculator");*/
+			
+			
+			
+			DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
+		    desiredCapabilities.setCapability("app", "/Users/rodrigolopes/Desktop/SimpleCalculator.app");
+		    desiredCapabilities.setCapability("deviceName", "iPhone 8 Plus");
+		    desiredCapabilities.setCapability("udid", "BE891D50-E3C0-4D9C-8922-0A6A9B6705A2");
+		    desiredCapabilities.setCapability("platformName", "ios");
+		    desiredCapabilities.setCapability("platformVersion", "14.4");
+		    desiredCapabilities.setCapability("showXcodeLog", true);
+		    desiredCapabilities.setCapability("includeSafariInWebviews", true);
+			
 
 			URL remoteUrl = new URL("http://localhost:4723/wd/hub");
 
-			driver = new AndroidDriver(remoteUrl, desiredCapabilities);
+			//driver = new AndroidDriver(remoteUrl, desiredCapabilities);
+			driver = new IOSDriver(remoteUrl, desiredCapabilities);
 		} catch (Throwable e) {
 			System.out.println("Error setUp: " + e.getMessage());
 		}
 	}
+	
 
 	@Test
 	@Order(1)
-	// Perform a Sum of 2 plus 2
+	// Perform a Sum of 10 plus 20
 	public void PerformSum() {
 		try {
 			// BrowserStack Setting name of test
@@ -55,27 +71,33 @@ public class ChallengeAppiumTests {
 			// test will fail if run lasts more than 5 minutes
 			Assertions.assertTimeoutPreemptively(Duration.ofMinutes(5), () -> {
 				
-				//get digit_2 element
-				MobileElement digit_2 = (MobileElement) driver.findElementById("com.android.calculator2:id/digit_2");
 				
-				//perform a click on digit_2
-				digit_2.click();
+				//get first_input element
+				MobileElement first_input = (MobileElement) driver.findElementByAccessibilityId("apple_first_input");
 				
-				//get plus element
-				MobileElement plus = (MobileElement) driver.findElementByAccessibilityId("plus");
+				//type a value on first_input
+				first_input.sendKeys("10");
 				
-				//perform a click on digit_2
-				plus.click();
+				//get second_input element
+			    MobileElement second_input = (MobileElement) driver.findElementByAccessibilityId("apple_second_input");
+			    
+			    //type a value on second_input
+			    second_input.sendKeys("20");
+			    
+			    //get sum_button element
+			    MobileElement sum = (MobileElement) driver.findElementByAccessibilityId("apple-sum-button");
+			    
+			    //perform a click on sum_button element
+			    sum.click();
+			    
+			    //get result text element
+			    MobileElement result_text = (MobileElement) driver.findElementByAccessibilityId("apple_result_text");
 				
-				//perform a click on digit_2
-				digit_2.click();
+			    //Validating that result exhibited is '30'
+				assertEquals("30", result_text.getText());
 				
-				//get result element
-				MobileElement result = (MobileElement) driver.findElementById("com.android.calculator2:id/result");
-
-				// Validating that result exhibited is '4'
-				assertEquals("4", result.getText());
-
+				
+				
 			});
 
 			// BrowserStack Test 'passed' label
@@ -90,7 +112,7 @@ public class ChallengeAppiumTests {
 			// "browserstack_executor: {\"action\": \"setSessionStatus\", \"arguments\":
 			// {\"status\":\"failed\"}}");
 
-			System.out.println("Error on Test createAccount: " + e.getMessage());
+			System.out.println("Error on Test PerformSum: " + e.getMessage());
 
 			assertTrue(false);
 		}
@@ -98,7 +120,7 @@ public class ChallengeAppiumTests {
 
 	@Test
 	@Order(2)
-	// Perform a Subtraction of 10 by 5
+	// Perform a Subtraction of 20 by 5
 	public void PerformSubtraction() {
 		try {
 			// BrowserStack Setting name of test
@@ -110,35 +132,29 @@ public class ChallengeAppiumTests {
 			// test will fail if run lasts more than 5 minutes
 			Assertions.assertTimeoutPreemptively(Duration.ofMinutes(5), () -> {
 				
-				// get digit_1 element
-				MobileElement digit_1 = (MobileElement) driver.findElementById("com.android.calculator2:id/digit_1");
+				//get first_input element
+				MobileElement first_input = (MobileElement) driver.findElementByAccessibilityId("apple_first_input");
 				
-				//perform a click on digit_1
-				digit_1.click();
+				//type a value on first_input
+				first_input.sendKeys("20");
 				
-				// get digit_0 element
-				MobileElement digit_0 = (MobileElement) driver.findElementById("com.android.calculator2:id/digit_0");
+				//get second_input element
+			    MobileElement second_input = (MobileElement) driver.findElementByAccessibilityId("apple_second_input");
+			    
+			    //type a value on second_input
+			    second_input.sendKeys("5");
+			    
+			    //get subtract-button element
+			    MobileElement subtract = (MobileElement) driver.findElementByAccessibilityId("apple-subtract-button");
+			    
+			    //perform a click on subtract_button element
+			    subtract.click();
+			    
+			    //get result text element
+			    MobileElement result_text = (MobileElement) driver.findElementByAccessibilityId("apple_result_text");
 				
-				//perform a click on digit_0
-				digit_0.click();
-				
-				// get minus element
-				MobileElement minus = (MobileElement) driver.findElementByAccessibilityId("minus");
-				
-				//perform a click on minus
-				minus.click();
-				
-				// get digit_5 element
-				MobileElement digit_5 = (MobileElement) driver.findElementById("com.android.calculator2:id/digit_5");
-				
-				//perform a click on digit_5
-				digit_5.click();
-				
-				// get result element
-				MobileElement result = (MobileElement) driver.findElementById("com.android.calculator2:id/result");
-
-				// Validating that result exhibited is '5'
-				assertEquals("5", result.getText());
+			    //Validating that result exhibited is '15'
+				assertEquals("15", result_text.getText());
 
 			});
 
@@ -174,35 +190,29 @@ public class ChallengeAppiumTests {
 			// test will fail if run lasts more than 5 minutes
 			Assertions.assertTimeoutPreemptively(Duration.ofMinutes(5), () -> {
 
-				// get digit_5 element
-				MobileElement digit_5 = (MobileElement) driver.findElementById("com.android.calculator2:id/digit_5");
-
-				// perform a click at digit_5
-				digit_5.click();
-
-				// get multiply element
-				MobileElement multiply = (MobileElement) driver.findElementByAccessibilityId("multiply");
-
-				// perform a click at multiply
-				multiply.click();
-
-				// get digit_2 element
-				MobileElement digit_2 = (MobileElement) driver.findElementById("com.android.calculator2:id/digit_2");
-
-				// perform a click at digit_1
-				digit_2.click();
-
-				// get digit_0 element
-				MobileElement digit_0 = (MobileElement) driver.findElementById("com.android.calculator2:id/digit_0");
-
-				// perform a click at digit_0
-				digit_0.click();
-
-				// get result element
-				MobileElement result = (MobileElement) driver.findElementById("com.android.calculator2:id/result");
-
-				// Validating that result exhibited is '100'
-				assertEquals("100", result.getText());
+				//get first_input element
+				MobileElement first_input = (MobileElement) driver.findElementByAccessibilityId("apple_first_input");
+				
+				//type a value on first_input
+				first_input.sendKeys("20");
+				
+				//get second_input element
+			    MobileElement second_input = (MobileElement) driver.findElementByAccessibilityId("apple_second_input");
+			    
+			    //type a value on second_input
+			    second_input.sendKeys("5");
+			    
+			    //get multiply-button element
+			    MobileElement multiply = (MobileElement) driver.findElementByAccessibilityId("apple-multiply-button");
+			    
+			    //perform a click on multiply_button element
+			    multiply.click();
+			    
+			    //get result text element
+			    MobileElement result_text = (MobileElement) driver.findElementByAccessibilityId("apple_result_text");
+				
+			    //Validating that result exhibited is '100'
+				assertEquals("100", result_text.getText());
 
 			});
 
@@ -238,38 +248,29 @@ public class ChallengeAppiumTests {
 			// test will fail if run lasts more than 5 minutes
 			Assertions.assertTimeoutPreemptively(Duration.ofMinutes(5), () -> {
 
-				// get button_1 element
-				MobileElement button_1 = (MobileElement) driver.findElementById("com.android.calculator2:id/digit_1");
-
-				// Perform a click at button_1
-				button_1.click();
-
-				// get button_0 element
-				MobileElement button_0 = (MobileElement) driver.findElementById("com.android.calculator2:id/digit_0");
-
-				// Perform a click at button_0
-				button_0.click();
-
-				// Perform a click at button_0
-				button_0.click();
-
-				// get button_divide element
-				MobileElement button_divide = (MobileElement) driver.findElementByAccessibilityId("divide");
-
-				// Perform a click at button_divide
-				button_divide.click();
-
-				// get button_4 element
-				MobileElement button_4 = (MobileElement) driver.findElementById("com.android.calculator2:id/digit_4");
-
-				// Perform a click at button_4
-				button_4.click();
-
-				// get result element
-				MobileElement result = (MobileElement) driver.findElementById("com.android.calculator2:id/result");
-
-				// Validating that result exhibited is '25'
-				assertEquals("25", result.getText());
+				//get first_input element
+				MobileElement first_input = (MobileElement) driver.findElementByAccessibilityId("apple_first_input");
+				
+				//type a value on first_input
+				first_input.sendKeys("100");
+				
+				//get second_input element
+			    MobileElement second_input = (MobileElement) driver.findElementByAccessibilityId("apple_second_input");
+			    
+			    //type a value on second_input
+			    second_input.sendKeys("4");
+			    
+			    //get multiply-button element
+			    MobileElement divide = (MobileElement) driver.findElementByAccessibilityId("apple-divide-button");
+			    
+			    //perform a click on divide_button element
+			    divide.click();
+			    
+			    //get result text element
+			    MobileElement result_text = (MobileElement) driver.findElementByAccessibilityId("apple_result_text");
+				
+			    //Validating that result exhibited is '25'
+				assertEquals("25", result_text.getText());
 
 			});
 
